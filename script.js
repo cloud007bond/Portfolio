8fcf9942f0958879397bb7141c72bedc7cb0a882
wac <wabaham9@gmail.com>
2023-06-30 17:12:30 +0300

    Clean the code

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const btn_hamburger = document.querySelector('.hamburger-img');
 const btn_close = document.querySelector('.close-1');
 
 const links = document.querySelectorAll('.lnks a');
-for (let link of links) {
-    link.addEventListener('click', () => {
-        modal_menu.close();
-    });
+for (const link of links) {
+  link.addEventListener('click', () => {
+    modal_menu.close();
+  });
 }
 
 btn_hamburger.addEventListener('click', () => {
-    modal_menu.show();
+  modal_menu.show();
 });
 
 btn_close.addEventListener('click', () => {
-    modal_menu.close();
+  modal_menu.close();
 });
